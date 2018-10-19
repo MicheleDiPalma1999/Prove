60d4be2d86a2252db60883dfacd66c41c4faca41
mikid <mikid@DESKTOP-6UM4TEU.homenet.telecomitalia.it>
2018-10-19 10:58:08 +0200

    Commit Prove2

--- src/prove/Prove2.java
+++ src/prove/Prove2.java
@@ -3,8 +3,19 @@ package prove;
 public class Prove2 {
 
     public static void main(String[] args) {
-        A a = new B();  
+        A a = new A();
         B b = new B();
+        C c = new C();
+
+        A[] v = {a, b, c};
+
+//        for (int i = 0; i < 3; i++) {
+//        System.out.println(v[i].calcola());
+//        }
+        for (A elemento : v) {
+            System.out.println(elemento.calcola());
+        }
+
     }
 
 }
@@ -14,11 +25,40 @@ class A {
     public A() {
         System.out.println("Sono A");
     }
+
+    public String calcola() {
+        return "Calcolo con modalità di A";
+    }
 }
 
 class B extends A {
-    
-    public B(){
+
+    public B() {
+        super();
         System.out.println("Sono B");
+    }
+
+    public void stampa() {
+        System.out.println("Stampo da B");
+    }
+
+    public String calcola() {
+        return "Calcolo con modalità di B";
+    }
+}
+
+class C extends B {
+
+    public C() {
+        super();
+        System.out.println("Sono C");
+    }
+
+    public void stampa() {
+        System.out.println("Stampo da C");
+    }
+
+    public String calcola() {
+        return "Calcolo con modalità di C";
     }
 }
